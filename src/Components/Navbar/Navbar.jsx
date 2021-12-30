2e0faf05b8cd7410432318f06a7f83058a3cfd06
Roby Morgan <roby.c.morgan@gmail.com>
2021-12-30 22:36:15 +0100

    useParams is undefined.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import { Container } from "react-bootstrap";
 import "./Navbar.css"
 import { BsFillCartFill } from "react-icons/bs"
 import Searchbar from "./Searchbar";
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
-        <Container fluid>
-            <div className="nav d-flex align-items-center justify-content-between" >
-                <div to="/" className="nav-link" href="/home">Home</div>
-                <div to="/cart" className="nav-link"><BsFillCartFill /></div>
+        <Container>
+            <div className="nav d-flex align-content-center" >
+                <Link to="/" className="nav-link" href="/home">Home</Link>
+                <Link to="/cart" className="nav-link"><BsFillCartFill /></Link>
                 <div className="search_bar " >
                     <Searchbar />
                 </div>

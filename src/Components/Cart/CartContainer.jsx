2d67cf5de2714bcff5ff06dfdbf96c860754e627
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-03 11:26:55 +0100

    added style to cart.

--- src/Components/Cart/CartContainer.jsx
+++ src/Components/Cart/CartContainer.jsx
@@ -1,5 +1,5 @@
 import "./Cart.css"
-import { Col } from "react-bootstrap"
+import { Row, Col, Table } from "react-bootstrap"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import Navbar from "../Navbar/Navbar"
@@ -7,6 +7,7 @@ import Navbar from "../Navbar/Navbar"
 const CartContainer = () => {
 
     const [cartItems, setCartItems] = useState(null)
+    const [totalSumPrice, setTotalsumPrice] = useState({})
 
     const fetchCartItems = async () => {
         try {
@@ -20,20 +21,74 @@ const CartContainer = () => {
             console.log(error)
         }
     }
+    // const fetchCartUnit = async () => {
+    //     try {
+    //         const response = await fetch("https://hw-m6d5.herokuapp.com/shoppingcart/16abcacb-d1ea-40d5-8a63-941a3a4a9fc4")
+    //         if (response.ok) {
+    //             const data = await response.json()
+    //             setTotalsumPrice(data)
+    //             console.log(data)
+    //         }
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
     useEffect(() => {
         fetchCartItems()
+        // fetchCartUnit()
     }, [])
 
     return (
         <>
             <Navbar />
             <h1>Cart</h1>
-            {
+
+            <Table>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>quantity</th>
+                    <th>Total</th>
+                </tr>
+                {
+                    cartItems?.map((item) => {
+                        return (
+                            <tr key={item.productid}>
+                                <td className="col-md-6">
+                                    <div className="column-flex"  >
+                                        <img className="product_img"
+                                            src={item.product.image_url} alt={item.product_name} />
+                                        <div>{item.product.product_name}</div>
+                                        <div>{item.product.description}</div>
+                                    </div>
+                                </td>
+                                <td className="col-md-2">
+                                    <h5>{item.product.price}</h5>
+                                </td>
+                                <td className="col-md-2">
+                                    <div className="d-flex" >
+                                        <button>+</button>
+                                        <div>{item.unitQuantity}</div>
+                                        <button>-</button>
+                                    </div>
+                                </td>
+                                <td className="col-md-2">
+                                    <div>{item.unitTotalPrice}</div>
+                                </td>
+
+                            </tr>
+
+                        )
+                    })
+                }
+
+            </Table>
+            {/* {
                 cartItems?.map((item) => {
                     return (
                         <section>
-                            <div key={item.productIde} className="product_headers d-flex" >
+                            <div key={item.productId} className="product_headers d-flex" >
                                 <Col className="flex-colum" md={3}>
                                     <div>Product</div>
                                     <img className="product_img" src={item.product.image_url} />
@@ -62,9 +117,17 @@ const CartContainer = () => {
                                 </Col>
                             </div>
                         </section>
+
                     )
                 })
-            }
+            } */}
+            <footer>
+                <Row>
+                    <div>
+                        Cart Total :
+                    </div>
+                </Row>
+            </footer>
         </>
     )
 }
